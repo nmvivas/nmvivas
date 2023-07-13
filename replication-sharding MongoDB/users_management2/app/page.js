2e9a7f4876ee91fbf63021090f40372de0774a2a
nmvivas <nmvivas@espe.edu.ec>
2023-07-12 23:56:45 -0500

    replication and sharding in MongoDB with docker

---
+++ replication-sharding MongoDB/users_management2/app/page.js
@@ -0,0 +1,349 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TextField from "@mui/material/TextField";
+
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box, Modal, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { ModeEdit } from "@mui/icons-material";
+import ClearIcon from "@mui/icons-material/Clear";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const rows = [
+  {
+    id: 1,
+    cedula: "111111111111",
+    nombre: "Admin",
+    email: "admin@gmail.com",
+    usuario: "admin",
+    pass: "admin123",
+  },
+  {
+    id: 2,
+    cedula: "222222222222",
+    nombre: "Admin",
+    email: "admin@gmail.com",
+    usuario: "admin",
+    pass: "admin123",
+  },
+  {
+    id: 3,
+    cedula: "333333333333",
+    nombre: "Admin",
+    email: "admin@gmail.com",
+    usuario: "admin",
+    pass: "admin123",
+  },
+];
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  borderRadius: "5px",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  p: 5,
+};
+
+export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+  const [selected, setSelected] = useState({
+    cedula: "",
+    nombre: "",
+    email: "",
+    usuario: "",
+    password: "",
+    _id: "",
+  });
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [newUserForm, setNewUserForm] = useState({
+    cedula: "",
+    nombre: "",
+    email: "",
+    usuario: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios("http://localhost:3003/users");
+      setUsers(result.data);
+    }
+
+    fetchData();
+  }, []);
+  console.log(newUserForm);
+  return (
+    <Box sx={{ padding: "35px" }}>
+      <Modal
+        open={showCreateModal}
+        onClose={() => {
+          setShowCreateModal(false);
+        }}
+      >
+        <Box sx={style}>
+          <Typography sx={{ marginBottom: "22px" }} variant="h4">
+            New User
+          </Typography>
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Cedula"
+            variant="standard"
+            value={newUserForm.cedula}
+            onChange={(e) => {
+              setNewUserForm({ ...newUserForm, cedula: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Nombre"
+            variant="standard"
+            value={newUserForm.nombre}
+            onChange={(e) => {
+              setNewUserForm({ ...newUserForm, nombre: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            value={newUserForm.email}
+            onChange={(e) => {
+              setNewUserForm({ ...newUserForm, email: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Usuario"
+            variant="standard"
+            value={newUserForm.usuario}
+            onChange={(e) => {
+              setNewUserForm({ ...newUserForm, usuario: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "45px" }}
+            id="standard-basic"
+            label="Contraseña"
+            variant="standard"
+            value={newUserForm.password}
+            onChange={(e) => {
+              setNewUserForm({ ...newUserForm, password: e.target.value });
+            }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "end",
+            }}
+          >
+            <Button
+              sx={{
+                marginRight: 2,
+                backgroundColor: "error.main",
+              }}
+              variant="contained"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:3003/users/create",
+                  newUserForm
+                );
+                console.log("asdasd");
+                console.log(response);
+
+                if (response.status === 200) {
+                  setShowCreateModal(false);
+                  setUsers([...users, newUserForm]);
+                }
+
+                console.log(response);
+              }}
+              sx={{
+                backgroundColor: "success.main",
+              }}
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Modal
+        open={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+      >
+        <Box sx={style}>
+          <Typography sx={{ marginBottom: "22px" }} variant="h4">
+            Edit User
+          </Typography>
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Cedula"
+            variant="standard"
+            value={selected.cedula}
+            onChange={(e) => {
+              setSelected({ ...selected, cedula: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Nombre"
+            variant="standard"
+            value={selected.nombre}
+            onChange={(e) => {
+              setSelected({ ...selected, nombre: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            value={selected.email}
+            onChange={(e) => {
+              setSelected({ ...selected, email: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "15px" }}
+            id="standard-basic"
+            label="Usuario"
+            variant="standard"
+            value={selected.usuario}
+            onChange={(e) => {
+              setSelected({ ...selected, usuario: e.target.value });
+            }}
+          />
+          <TextField
+            sx={{ width: "100%", marginBottom: "45px" }}
+            id="standard-basic"
+            label="Contraseña"
+            variant="standard"
+            value={selected.password}
+            onChange={(e) => {
+              setSelected({ ...selected, password: e.target.value });
+            }}
+          />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "end",
+            }}
+          >
+            <Button
+              sx={{
+                marginRight: 2,
+                backgroundColor: "error.main",
+              }}
+              variant="contained"
+            >
+              Cancel
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "success.main",
+              }}
+              onClick={async () => {
+                const response = await axios.post(
+                  "http://localhost:3003/users/edit",
+                  { ...selected }
+                );
+
+                if (response.status === 200) {
+                  setUsers(
+                    users.map((u) => {
+                      if (u._id === selected._id) {
+                        u = { ...selected };
+                      }
+                      return u;
+                    })
+                  );
+                  setShowModal(false);
+                }
+              }}
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginBottom: "25px",
+        }}
+      >
+        <Typography variant="h3">Reading Users Management</Typography>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: "600" }}>Cedula</TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right">
+                Nombre
+              </TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right">
+                Email
+              </TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right">
+                Usuario
+              </TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right">
+                Contraseña
+              </TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right"></TableCell>
+              <TableCell sx={{ fontWeight: "600" }} align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.cedula}
+                </TableCell>
+                <TableCell align="right">{row.nombre}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.usuario}</TableCell>
+                <TableCell align="right">{row.password}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
---
+++ replication-sharding MongoDB/users_management_api/server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+const url = "mongodb://localhost:60000";
+// const client = new MongoClient(url);
+const client = new MongoClient(
+  "mongodb://localhost:50001," + "localhost:50002," + "localhost:50003"
+);
+const cors = require("cors");
+
+const dbName = "sharddemo";
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.post("/users/create", async (req, res) => {
+  const { cedula, nombre, email, usuario, password } = req.body;
+
+  const db = client.db(dbName);
+  const collection = db.collection("myusers");
+  try {
+    const result = await collection.insertOne({
+      cedula,
+      nombre,
+      email,
+      usuario,
+      password,
+    });
+
+    return res.status(200).send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/edit", async (req, res) => {
+  const { cedula, nombre, email, usuario, password, _id } = req.body;
+
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("myusers");
+    const result = await collection.updateOne(
+      { _id: new ObjectId(_id), usuario },
+      { $set: { cedula, nombre, email, usuario, password } }
+    );
+    return res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send();
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("myusers");
+    const result = await collection.find({}).toArray();
+    return res.json(result).status(200).send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/delete", async (req, res) => {
+  const { _id } = req.body;
+
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("myusers");
+    const result = await collection.deleteOne({ _id: new ObjectId(_id) });
+    return res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send();
+  }
+});
+
+app.listen(3001, () => {
+  console.log("app started on port 3001");
+});
